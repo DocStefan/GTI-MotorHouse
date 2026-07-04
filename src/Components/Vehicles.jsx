b7a8a520c80aecc3bbbb91d8471b33c994f77cc1
Stefano_Minici <stefaminici@gmail.com>
2026-07-04 18:20:09 -0300

    picsFixC

--- src/Components/Vehicles.jsx
+++ src/Components/Vehicles.jsx
@@ -363,7 +363,7 @@ useEffect(() => {Starter()}, [])
                             return <div className="CataloguePost">
                                 <div className="CalaloguePostBox">
 
-                                    <img loading='eager' decoding='async' className="PostPic" src={val.foto} >
+                                    <img loading='lazy' decoding='async' className="PostPic" src={val.foto} >
                                     </img>
 
                                     <div className="FavMarkPost" id={index}><span class="material-symbols-outlined FavPost">bookmark</span></div>
